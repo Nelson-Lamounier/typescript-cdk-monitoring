46acfd1340379bdc15fa366c305be3548c588690
Nelson Lamounier <lamounierleao@gmail.com>
2026-01-09 11:05:47 +0000

    test(ecr-construct): add unit tests for EcrConstruct
    - Introduced unit tests for the EcrConstruct to validate the creation of ECR repositories with default settings, tagging, lifecycle rules, and replication configurations.
    - Ensured proper resource properties and configurations are tested for both default and replication scenarios.
    
    Files Added:
    - tests/unit/constructs/storage/ecr-construct.test.ts

---
+++ tests/unit/constructs/storage/ecr-construct.test.ts
@@ -0,0 +1,88 @@
+/** @format */
+
+import * as cdk from "aws-cdk-lib";
+import { Template, Match } from "aws-cdk-lib/assertions";
+
+import { EcrConstruct } from "../../../../lib/constructs/storage/ecr/ecr-construct";
+
+describe("EcrConstruct", () => {
+  let app: cdk.App;
+  let stack: cdk.Stack;
+
+  beforeEach(() => {
+    app = new cdk.App();
+    stack = new cdk.Stack(app, "TestStack", {
+      env: { account: "123456789012", region: "eu-west-1" },
+    });
+  });
+
+  test("creates repository with defaults, tagging, and lifecycle rule", () => {
+    new EcrConstruct(stack, "EcrRepo", {
+      envName: "dev",
+      projectName: "proj",
+      repositoryName: "my-repo",
+    });
+
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs("AWS::ECR::Repository", 1);
+    template.hasResourceProperties("AWS::ECR::Repository", {
+      RepositoryName: "my-repo",
+      ImageScanningConfiguration: { ScanOnPush: true },
+      ImageTagMutability: "IMMUTABLE",
+    });
+
+    template.hasResourceProperties("AWS::ECR::Repository", {
+      LifecyclePolicy: {
+        LifecyclePolicyText: Match.serializedJson(
+          Match.objectLike({
+            rules: Match.arrayWith([
+              Match.objectLike({
+                rulePriority: 1,
+                selection: Match.objectLike({
+                  tagStatus: "any",
+                  countNumber: 10,
+                }),
+              }),
+            ]),
+          })
+        ),
+      },
+    });
+  });
+
+  test("creates replication configuration when destinations provided", () => {
+    const repo = new EcrConstruct(stack, "EcrWithReplication", {
+      envName: "dev",
+      repositoryName: "my-repo",
+      replicationDestinations: [
+        {
+          region: "eu-west-2",
+        },
+      ],
+    });
+
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs("AWS::ECR::ReplicationConfiguration", 1);
+    template.hasResourceProperties("AWS::ECR::ReplicationConfiguration", {
+      ReplicationConfiguration: {
+        Rules: [
+          Match.objectLike({
+            Destinations: [
+              Match.objectLike({
+                Region: "eu-west-2",
+                RegistryId: "123456789012",
+              }),
+            ],
+            RepositoryFilters: [
+              Match.objectLike({
+                FilterType: "PREFIX",
+              }),
+            ],
+          }),
+        ],
+      },
+    });
+  });
+});
